09669b337c9d0d8ebfe0c3d72fbdf4599732cf74
Yaroslav Malykhin <sk3llyar@gmail.com>
2024-04-22 23:54:50 +0300

    fix: release.config.cjs

---
+++ release.config.cjs
@@ -0,0 +1,17 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+    branches: ['main'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/npm',
+        '@semantic-release/github',
+        ['@semantic-release/git', {
+            assets: ['package.json', 'CHANGELOG.md'],
+            message: 'chore(release): ${nextRelease.version}'
+        }]
+    ]
+};
